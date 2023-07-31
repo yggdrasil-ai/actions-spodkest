4384727ae86326bf13ffabfdde5ac57289d91aac
Santiago Rincon <santiago.rincon@inlabdigital.com>
2023-07-31 13:22:18 +0200

    New: fix typo

--- spodkast_event_manager/main.py
+++ spodkast_event_manager/main.py
@@ -359,7 +359,7 @@ def _export_spodkast(author, spodkast_id, payload):
     mail = read_file(f"{assigned_folder}/mail.txt")
     podcast = read_bytes(f"{assigned_folder}/podcast.mp3")
     write_bytes(f"{destiny_folder}/podcast.mp3", podcast)
-    message = f"""#c#send-mail#|#author={USER}#|#body=Your podcast have been produced, you can download it using this link: https://{export_route}/podcast.mp3#|#to={mail}#|#subject=Produced podcast from your PDF#|#from=PDFtoPodcast#c#"""
+    message = f"""#c#send-mail#|#author={USER}#|#body=Your podcast have been produced, you can download it using this link: https://storage.googleapis.com/{export_route}/podcast.mp3#|#to={mail}#|#subject=Produced podcast from your PDF#|#from=PDFtoPodcast#c#"""
     payload = json.dumps({
         'author': USER,
         'role': 'user',
